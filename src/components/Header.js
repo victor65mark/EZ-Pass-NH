3a1d05234ffcfc4d334c0669680c53a4df82aed7
victor65mark <victor65mark@outlook.com>
2025-11-03 17:12:09 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>EZ Pass NH</h1>
+      <p>Manage your tolls and transactions effortlessly</p>
+    </header>
+  );
+}
+
+export default Header;
